0d5c08ea3705c4d6d678c651ceaa9b9f7b11e7bc
pankajmahtolia <34932851+pankajmahtolia@users.noreply.github.com>
2018-02-22 11:46:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/article-one', function (req, res) {
+app.get('/ui/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/atticle-two', function (req, res) {
