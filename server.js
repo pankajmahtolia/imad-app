211dd0b27640c84483d2abaf4a5c98608f7791ba
pankajmahtolia <34932851+pankajmahtolia@users.noreply.github.com>
2018-02-22 21:28:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
    var articleName=req.params.articleName;
-  res.send(templete(articles([articleName])));
+  res.send(createtemp(articles([articleName])));
 });
 
 app.get('/ui/style.css', function (req, res) {
