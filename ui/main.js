93f7cdb1ec8150e9811191a4d5e1ff37a1d3f108
pankajmahtolia <34932851+pankajmahtolia@users.noreply.github.com>
2018-02-23 17:26:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+//changing text of main page
+var element=document.getElementById(`centre_text`);
+element.innerHTML=`Hey, Everyone It's Pankaj@Ash Here!`;
+
+//for moving madi (image)
+var img = document.getElementById(`madi`);
+img.onclick =function(){
+    img.style.marginLeft='50px';
+};
