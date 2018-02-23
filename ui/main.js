7e6f6326605d9ce096c2f6c84738cf51616d460b
pankajmahtolia <34932851+pankajmahtolia@users.noreply.github.com>
2018-02-23 20:38:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,14 @@ console.log('Loaded GameOn!');
 var img = document.getElementById(`img`);
 var marginLeft=0;
 function moveRight(){
+    
     marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';   
+    img.style.marginLeft=marginLeft+'px';
+    
     
 }
 img.onclick =function(){
+    if(marginLeft<=200)
     var interval=setInterval(moveRight,10);
 
 };
