13da41c46f04109ebf29114a42f5fb1b580a8834
pankajmahtolia <34932851+pankajmahtolia@users.noreply.github.com>
2018-02-23 19:31:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,4 @@
 console.log('Loaded GameOn!');
-//changing text of main page
-var element=document.getElementById(`centre_text`);
-element.innerHTML=`Hey, Everyone It's Pankaj@Ash Here!`;
 
 //for moving madi (image)
 var img = document.getElementById(`madi`);
