c2557fb60df6162684acca49603e42c346eb4520
pankajmahtolia <34932851+pankajmahtolia@users.noreply.github.com>
2018-02-24 14:52:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,18 +11,18 @@ img.onclick =function(){
 
 };
 function moveRight(){
-    if(marginRight>=0&&marginRight<=150)
+    if(marginRight>=0&&marginRight<=100)
         {marginRight++;
     marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+'px';
             
         }
-    else if(marginRight>150&&marginRight<=450){
+    else if(marginRight>101&&marginRight<=300){
     marginRight++;
     marginLeft=marginLeft-1;
     img.style.marginLeft=marginLeft+'px';
     }
-    else if(marginRight>450){
+    else if(marginRight>300){
     marginRight=0;    
     marginLeft=0;
     }
