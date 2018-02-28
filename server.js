507636dff934dcfdecc710575782a70812e9d4c9
pankajmahtolia <34932851+pankajmahtolia@users.noreply.github.com>
2018-03-01 01:11:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/myarticles/:articleName', function (req, res) {
                 if(result.rows.length === 0){
                 res.status(404).send('myarticles not found');}
                 else{
-                    var articleData=result.rows[0];
+                    var articleData = result.rows[0];
                       res.send(createtemp(articleData));
                 }
             }
