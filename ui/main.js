c309214e38cc2cd86246e05ae4198830125b3a1a
pankajmahtolia <34932851+pankajmahtolia@users.noreply.github.com>
2018-03-01 15:54:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,39 @@
 console.log('Loaded GameOn!');
 
+// click Me button
+
+var button = document.getElementById('counter');
+
+//on click
+button.onclick = function(){
+  // creating a request to browsre
+  var request = new XMLHttpRequest();
+
+  
+  //condition of request
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.Done){
+      //successsful or not
+      if(request.status === 200){
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+          
+        }
+      //else 
+     
+      }
+    };
+  
+    // before going to onreadystate function it will make a request
+    request.open('GET','http://pankajmahtolia0.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+
+
+
+
+
 //for moving (image)
 var img = document.getElementById(`img`);
 var marginLeft=0;
@@ -29,35 +63,4 @@ function moveRight(){
 
     
 }
-
-// click Me button
-
-var button = document.getElementById('counter');
-
-//on click
-button.onclick = function(){
-  // creating a request to browsre
-  var request = new XMLHttpRequest();
-
-  
-  //condition of request
-  request.onreadystatechange=function(){
-      if(request.readyState===XMLHttpRequest.Done){
-      //successsful or not
-      if(request.status === 200){
-          var counter=request.responseText;
-          var span=document.getElementById('count');
-          span.innerHTML=counter.toString();
-          
-        }
-      //else 
-     
-      }
-    };
-  
-    // before going to onreadystate function it will make a request
-    request.open('GET','http://pankajmahtolia0.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
 
